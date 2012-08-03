7e48fd3e6182b88d51f9b8223979c63ee9ee7283
marchand <marchand@LO-EIN-178B-JMA.ing.intra.eiaj.ch>
2012-08-03 17:05:52 +0200

    modif println

--- TestGit_01b/src/test/TestMain.java
+++ TestGit_01b/src/test/TestMain.java
@@ -7,7 +7,7 @@ public class TestMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("First Git commit :) 2012/08/03 17:02");
 	}
 
 }
