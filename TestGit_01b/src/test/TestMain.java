a194b1e9332176960f0b47739d28279be264aee5
marchand <marchand@LO-EIN-178B-JMA.ing.intra.eiaj.ch>
2012-08-03 17:35:54 +0200

    comment 2

--- TestGit_01b/src/test/TestMain.java
+++ TestGit_01b/src/test/TestMain.java
@@ -8,6 +8,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First GitHub commit :) 2012/08/03 17:02");
+		
+		// 2012/08/03 17:35 -> comment 2 
 	}
 
 }
