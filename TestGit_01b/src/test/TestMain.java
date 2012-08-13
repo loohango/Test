391229dca26112da44909f67578a3181bed07fe1
jma <loohango@gmail.com>
2012-08-13 21:31:12 +0200

    asdf

--- TestGit_01b/src/test/TestMain.java
+++ TestGit_01b/src/test/TestMain.java
@@ -7,9 +7,9 @@ public class TestMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i, n = 10;
+		int i, n = 1000;
 		for (i = 0; i < n; i++) {
-			System.out.println("First GitHub commit :) 2012/08/04 13:22");
+			System.out.println("First GitHub commit :) 2012/08/04 13:22 - " + i);
 		}
 		// 2012/08/03 17:35 -> comment 2 
 		// 2012/08/03 23:50 -> comment 3
